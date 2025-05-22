40756168568f37c7a22471c34371905e441edf14
yura <128141784+triplef-6@users.noreply.github.com>
2025-05-22 15:55:16 +0600

    0.33

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
 import {AppRoutes} from "@/app/routing";
 import {StrictMode} from "react";
-import {useOnboardingRedirect} from "@/app/routing/useOnboardingRedirect.ts";
 import {useAddManyFavFactory} from "@/features";
 
 export const App = () => {
 
-    useOnboardingRedirect()
+    //useOnboardingRedirect()
     useAddManyFavFactory()
 
     return (
--- src/app/routing/useOnboardingRedirect.ts
+++ src/app/routing/useOnboardingRedirect.ts
@@ -17,6 +17,6 @@ export const useOnboardingRedirect = () => {
             navigate(`/${RouteNames.ON_BOARDING}`)
         }
 
-    }, [])
+    }, [isAuth, navigate])
 
 }
